64b8fbb941852b65d784e0eb2dba7072437d49cb
shahbazcse <cseshahbaz@gmail.com>
2024-03-09 01:17:33 +0530

    Fixed Responsiveness

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -8,7 +8,7 @@ const Dashboard = () => {
     const [menu, setMenu] = useState("booking_history")
     return (
         <div className="mx-auto max-w-[91rem]">
-            <div className="mx-auto min-h-[44rem] max-w-[64rem] mt-20 mb-24 rounded-t-3xl border shadow-md">
+            <div className="mx-auto min-h-[44rem] max-w-[64rem] my-4 md:mt-20 md:mb-24 rounded-t-3xl border shadow-md">
                 <div className="flex items-center justify-start sm:justify-end overflow-auto gap-4 p-6 border-b">
                     <Button onClick={() => setMenu("booking_history")} variant={menu === "booking_history" ? "default" : "outline"} className="px-4 py-2 rounded-full shadow-sm">Booking History</Button>
                     <Button onClick={() => setMenu("favourites")} variant={menu === "favourites" ? "default" : "outline"} className="px-4 py-2 rounded-full shadow-sm">Favourites</Button>
